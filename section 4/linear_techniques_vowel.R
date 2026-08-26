09c0e43f68ac10b23729dc79b3327846a0a88901
Flavien De Bock <65034121+flaviendebock@users.noreply.github.com>
2026-08-26 20:53:13 +0200

    Add files via upload

---
+++ section 4/linear_techniques_vowel.R
@@ -0,0 +1,62 @@
+rm(list = ls())
+library(readr) ; library(tidyverse) ; library(corrplot)
+
+train <- read.csv("Linear Methods for Classification/vowel_train.csv", sep = ",")
+train$x.10. <- gsub("\\\\", "", train$x.10.)
+train$x.10. <- as.numeric(gsub("}", "", train$x.10.))
+train$x.10 <- train$x.10. ; train$x.10. <- NULL
+
+test <- read.csv("Linear Methods for Classification/vowel_test.csv", sep = ",")
+test$x.10. <- gsub("\\\\", "", test$x.10.)
+test$x.10. <- as.numeric(gsub("}", "", test$x.10.))
+test$x.10 <- test$x.10. ; test$x.10. <- NULL
+head(train) ; summary(train)
+
+corr_matrix <- cor(train[,-1]) # correlation matrix
+corrplot(corr_matrix, method = "square", addCoef.col = 'black')  # associated plot
+
+train[paste0("y_", 1:11)] <- lapply(1:11, function(k) { as.integer(train$y == k)})
+test[paste0("y_", 1:11)] <- lapply(1:11, function(k) { as.integer(test$y == k)})
+
+x_vars <- paste0("x.", 1:10) # string vector of the name of the features
+
+models <- vector("list", 11) # create an empty list to store the 11 regressions
+
+for (k in 1:11) { # loop through the 11 groups
+  y_var <- paste0("y_", k) # string vector of the name of group k 
+  
+  formula_k <- as.formula(
+    paste(y_var, "~", paste(x_vars, collapse = " + ")) # formula regress y_k on the features
+  )
+  
+  models[[k]] <- lm(formula_k, data = train) # fit the reg
+}
+
+fitted_train <- as.data.frame(
+  lapply(models, fitted) # retrieve the fitted values for each model
+) # we store them in a data.frame
+
+names(fitted_train) <- paste0("y_", 1:11) # rename the columns for clarity
+
+# the predicted outcome is the one with the highest fitted values 
+prediction_train <- max.col(fitted_train, ties.method = "first")
+
+# Training error rate is ~0.48
+train_error_rate <- mean(train$y != prediction_train) ; train_error_rate
+
+# we obtain the fitted values of the model on the test data
+fitted_test <- as.data.frame(
+  lapply(models, function(model) predict(model, newdata = test))
+)
+
+names(fitted_test) <- paste0("y_", 1:11)
+
+prediction_test <- max.col(fitted_test, ties.method = "first")
+
+# the test error rate is ~0.67
+test_error_rate <- mean(test$y != prediction_test) ; test_error_rate
+
+
+
+
+
